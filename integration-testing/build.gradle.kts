7e92a19bc148bcec06be4e8636e34ad989cb8bf9
René Knop <rene@kn0p.family>
2024-06-05 22:22:38 +0200

    Implement integration-testing gradle project

---
+++ integration-testing/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    `java-gradle-plugin`
+}
+
+repositories {
+    mavenLocal()
+    myCustomRepo()
+}
+
+gradlePlugin {
+    plugins {
+        create("BuildScriptRepositoriesAssertion") {
+            id = "buildScriptRepositoriesAssertion"
+            implementationClass = "org.example.BuildScriptRepositoriesAssertionPlugin"
+        }
+        create("DependencyResolutionManagementRepositoriesAssertionPlugin") {
+            id = "dependencyResolutionManagementRepositoriesAssertionPlugin"
+            implementationClass = "org.example.DependencyResolutionManagementRepositoriesAssertionPlugin"
+        }
+        create("PluginRepositoriesAssertionPlugin") {
+            id = "pluginRepositoriesAssertionPlugin"
+            implementationClass = "org.example.PluginRepositoriesAssertionPlugin"
+        }
+        create("ProjectRepositoriesAssertionPlugin") {
+            id = "projectRepositoriesAssertionPlugin"
+            implementationClass = "org.example.ProjectRepositoriesAssertionPlugin"
+        }
+        create("PublishingRepositoriesAssertionPlugin") {
+            id = "publishingRepositoriesAssertionPlugin"
+            implementationClass = "org.example.PublishingRepositoriesAssertionPlugin"
+        }
+    }
+}
+
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testImplementation(gradleTestKit())
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+fun RepositoryHandler.myCustomRepo(): MavenArtifactRepository =
+    maven {
+        val artifactoryAccessToken = System.getProperty("gradle.wrapperPassword")
+        url = uri("https://repo.maven.apache.org/maven2/") // url of my custom repo
+        name = "MyCustomRepo"
+        credentials(HttpHeaderCredentials::class) {
+            name = "Authorization"
+            value = "Bearer $artifactoryAccessToken"
+        }
+        authentication {
+            create<HttpHeaderAuthentication>("header")
+        }
+    }
